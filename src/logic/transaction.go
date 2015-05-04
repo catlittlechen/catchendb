84ee268a89bc068ab1afa6961f2dbfe0226d049c
catchen <catlittlechen@gmail.com>
2015-05-04 22:38:47 +0800

    save

--- src/logic/transaction.go
+++ src/logic/transaction.go
@@ -1,9 +1,34 @@
 package logic
 
 import (
+	"catchendb/src/node"
 	"catchendb/src/util"
 	"net/url"
+	"sync"
 )
+
+var (
+	transactionID    int
+	transactionMutex *sync.Mutex
+)
+
+func getTransactionId() (id int) {
+	transactionMutex.Lock()
+	transactionID += 1
+	id = transactionID
+	transactionMutex.Unlock()
+	return
+}
+
+type transaction struct {
+	ID        int
+	ChangeLog []transactionLog
+}
+
+type transactionLog struct {
+	typ  int
+	data node.Data
+}
 
 func handleBegin(keyword url.Values) []byte {
 	rsp := Rsp{}
@@ -30,4 +55,9 @@ func initTransaction() {
 	registerCMD(CMD_BEGIN, 1, handleBegin, TYPE_W)
 	registerCMD(CMD_COMMIT, 1, handleCommit, TYPE_W)
 	registerCMD(CMD_ROLLBACK, 1, handleRollBack, TYPE_W)
+}
+
+func init() {
+	transactionID = 0
+	transactionMutex = new(sync.Mutex)
 }
